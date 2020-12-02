960d938db13ed7b93cb0ab047f9134dc8b6f0cec
Janice-M <janiceink001@gmail.com>
2020-12-02 22:47:17 +0300

    fasti

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const PORT = process.env.PORT || 3000;
 const app = fastify({
   logger: true
 })
+
+const fastify = require('fastify'); //Bring in Fastify
+const PORT = process.env.PORT || 3000;
+const app = fastify({
+  logger: true
+})

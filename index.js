694e3636e49d8b1e99bd5043df11f31cfd87c954
Janice-M <janiceink001@gmail.com>
2020-12-02 22:47:46 +0300

    fasti

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ const PORT = process.env.PORT || 3000;
 const app = fastify({
     logger: true
 })
+//Funtion To run the server
+const start = async () => {
+    try {
+      await app.listen(PORT)
+      app.log.info(`server listening on ${app.server.address().port}`)
+    } catch (err) {
+      app.log.error(err)
+      process.exit(1)
+    }
+  }
